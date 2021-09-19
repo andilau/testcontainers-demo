8f6a3379c9a21b5e7b8364ddb4584f5b64b3ade6
Andi Lau <andi@herrlau.de>
2021-09-19 18:50:10 +0200

    Mod CustomerIntegrationTests use MysqlSQLContainer

--- src/test/java/com/jetbrains/testcontainersdemo/CustomerIntegrationTests.java
+++ src/test/java/com/jetbrains/testcontainersdemo/CustomerIntegrationTests.java
@@ -3,16 +3,28 @@ package com.jetbrains.testcontainersdemo;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@Testcontainers
 public class CustomerIntegrationTests {
 
     @Autowired
     private CustomerDao customerDao;
+
+    @Container
+    private static MySQLContainer container = new MySQLContainer("mysql:latest")
+            .withDatabaseName("somedatabase")
+            .withUsername("root")
+            .withPassword("letsgomarco");
 
     @Test
     void when_using_a_clean_db_this_should_be_empty() {
